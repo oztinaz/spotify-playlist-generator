671026a29605c058ed34f56f5c2f84a0df27f52b
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2023-12-30 19:00:47 +0300

    Redirect if not authorized

--- src/views/spotify/AuthorizationView.vue
+++ src/views/spotify/AuthorizationView.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-// TODO: Use authorization code to retrieve access token
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+import { useSpotifyTokenStore } from '@/stores/spotify/token'
+import type { SpotifyAccessToken } from '@/types/spotify/Token'
+import { CookieUtils } from '@/utils/Cookie'
+
+const route = useRoute()
+
+const spotifyTokenStore = useSpotifyTokenStore()
+const { fetchAccessToken } = spotifyTokenStore
+
+onMounted(async () => {
+  if (route.query.code) {
+    const spotifyAccessToken: SpotifyAccessToken = await fetchAccessToken(route.query.code as string)
+    
+    const expiresAt = new Date()
+    expiresAt.setTime(expiresAt.getTime() + spotifyAccessToken.expires_in * 1000)
+
+    CookieUtils.set('X-SPOTIFY-ACCESS-TOKEN', spotifyAccessToken.access_token, expiresAt)
+    CookieUtils.set('X-SPOTIFY-REFRESH-TOKEN', spotifyAccessToken.refresh_token)
+  }
+})
 </script>
 
 <template>
-  <div id="authorization-view">Authorization View</div>
+  <div id="authorization-view">
+    Authorizing...
+  </div>
 </template>
 
 <style scoped></style>
