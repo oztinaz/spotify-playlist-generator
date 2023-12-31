0f5f7dd003de50f3e4c5bebd8d293e92d4cf99d5
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2023-12-31 15:30:56 +0300

    Add get method to AxiosUtils

--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -1,6 +1,26 @@
 import axios from 'axios'
 
 export class AxiosUtils {
+  public static get<T>(
+    url: string,
+    headers: { [key: string]: any } = {},
+    params: { [key: string]: any } = {}
+  ): Promise<T> {
+    return new Promise<T>(async (resolve, reject) => {
+      await axios
+        .get(url, {
+          headers: headers,
+          params: params
+        })
+        .then((resp: { data: T }) => {
+          resolve(resp.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  }
+
   public static post<T>(
     url: string,
     headers: { [key: string]: any } = {},
